b057694f7f261ac5cc63aff456f67193c3acf1c6
Welton Vaz <weltonvaz@gmail.com>
2019-11-04 13:19:27 -0300

    Trabalho - 01/11/2019 - update do problemas 1005

--- 1005.go
+++ 1005.go
@@ -7,5 +7,6 @@ func main() {
 	var A, B float64
 	fmt.Scanln(&A)
 	fmt.Scanln(&B)
-	fmt.Println("MEDIA =", ((3.5*A)+(7.5*B))/(3.5+7.5))
+	s := fmt.Sprintf("%.5f", ((3.5*A)+(7.5*B))/(3.5+7.5))
+	fmt.Println("MEDIA =", s)
 }
